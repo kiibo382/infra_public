c858d347b67c1272089d1e713d334e7e89a5bef8
kiibo <shomok0106k@gmail.com>
2021-02-01 08:49:33 +0900

    add: package.json

---
+++ algorithm/01.py
@@ -0,0 +1,17 @@
+#%%
+
+N = int(input())
+
+divide_list = [int(input()) for _ in range(N)]
+
+print(divide_list)
+
+for i in divide_list:
+    if i % 2 == 0:
+        
+    else:
+        break
+
+print(count)
+
+#%%
